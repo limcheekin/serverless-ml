f6c546c14c66c4f549a8745a6bce9f6767e5c723
Lim Chee Kin <limcheekin@vobject.com>
2021-03-16 15:10:19 +0800

    added zip config and unzip code

--- spacy-example/handler.py
+++ spacy-example/handler.py
@@ -1,10 +1,11 @@
 print('container start')
 
-#try:
-#    import unzip_requirements
-#except ImportError as error:
-#    pass
-#print('unzipped')
+try:
+    import unzip_requirements
+except ImportError as error:
+    print(error)
+    
+print('unzipped')
 
 import json
 #import spacy
