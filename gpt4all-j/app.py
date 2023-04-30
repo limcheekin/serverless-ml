8b77cc451f4e044d6b424a0f9450aa4eb04d166c
Lim Chee Kin <limcheekin@vobject.com>
2023-04-30 21:37:43 +0800

    feat: added gpt4all-j custom image

--- gpt4all-j/app.py
+++ gpt4all-j/app.py
@@ -1,5 +1,41 @@
-import sys
+from gpt4allj import Model
+
+import json
+
+print('loading model...')
+
+# try:
+model = Model('./model/ggml-gpt4all-j')
+
+# except:
+print("An exception occurred on loading model.")
+
+print('model loaded\n')
 
 
 def handler(event, context):
-    return 'Hello from AWS Lambda using Python' + sys.version + '!'
+    body = {
+        "message": "OK",
+    }
+
+    if event.get("source") == "serverless-plugin-warmup":
+        body['message'] = 'WarmUP - Keep the Lambda warm!'
+
+    else:
+        data = json.loads(event['body'])
+        print("data['prompt']", data['prompt'])
+        result = model.generate(data['prompt'])
+        print(f"result: {result}")
+        body = {
+            "message": result,
+        }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body),
+        "headers": {
+            "Content-Type": "application/json"
+        }
+    }
+
+    return response
