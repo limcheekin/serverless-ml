658191d5416ca6f9af12dbfc835bd54b8096b947
Lim Chee Kin <limcheekin@vobject.com>
2023-05-03 16:03:08 +0800

    feat: updated code to support parameters setting

--- gpt4all-j/app.py
+++ gpt4all-j/app.py
@@ -12,6 +12,18 @@ except:
 
 print('model loaded\n')
 
+default_params: dict = {
+    "seed": -1,
+    "n_threads": -1,
+    "n_predict": 200,
+    "top_k": 40,
+    "top_p": 0.9,
+    "temp": 0.9,
+    "repeat_penalty": 1,
+    "repeat_last_n": 64,
+    "n_batch":  8
+}
+
 
 def handler(event, context):
     body = {
@@ -23,8 +35,23 @@ def handler(event, context):
 
     else:
         data = json.loads(event['body'])
-        print("data['prompt']", data['prompt'])
-        result = model.generate(data['prompt'])
+        print("prompt", data['prompt'])
+        print("params", data['params'])
+        if data['params']:
+            default_params.update(data['params'])
+            print("updated default_params", default_params)
+        result = model.generate(
+            prompt=data['prompt'],
+            seed=default_params["seed"],
+            n_threads=default_params["n_threads"],
+            n_predict=default_params["n_predict"],
+            top_k=default_params["top_k"],
+            top_p=default_params["top_p"],
+            temp=default_params["temp"],
+            repeat_penalty=default_params["repeat_penalty"],
+            repeat_last_n=default_params["repeat_last_n"],
+            n_batch=default_params["n_batch"]
+        )
         print(f"result: {result}")
         body = {
             "message": result,
