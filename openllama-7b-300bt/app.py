c8e980ce40cdd424b31d5930943b28d51057b5fc
Lim Chee Kin <limcheekin@vobject.com>
2023-05-05 20:46:35 +0800

    feat: added openllama-7b-300bt model

---
+++ openllama-7b-300bt/app.py
@@ -0,0 +1,39 @@
+from llama_cpp import Llama
+import json
+
+print('loading model...')
+
+try:
+    llm = Llama(model_path="./model/openllama-7b-300bt-ggml-q8_0.bin")
+except:
+    print("An exception occurred on loading model.")
+
+print('model loaded\n')
+
+
+def handler(event, context):
+    body = {
+        "message": "OK",
+    }
+
+    if event.get("source") == "serverless-plugin-warmup":
+        body['message'] = 'WarmUP - Keep the Lambda warm!'
+
+    else:
+        data = json.loads(event['body'])
+        print("data['prompt']", data['prompt'])
+        result = llm(data['prompt'], max_tokens=32, stop=["Q:", "\n"], echo=True)        result = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+        print(f"result: {result}")
+        body = {
+            "message": result,
+        }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body),
+        "headers": {
+            "Content-Type": "application/json"
+        }
+    }
+
+    return response
