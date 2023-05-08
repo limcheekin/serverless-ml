1027ee4c0fca31c98765e4f5e31e0858e6caf435
Lim Chee Kin <limcheekin@vobject.com>
2023-05-08 10:43:57 +0800

    feat: updated n_thread to use cpu_count and max out the memory size to 10240

--- openllama-7b-300bt/app.py
+++ openllama-7b-300bt/app.py
@@ -1,10 +1,12 @@
 from llama_cpp import Llama
 import json
+import multiprocessing
 
 print('loading model...')
-
+cpu_count = multiprocessing.cpu_count()
+print("cpu_count", cpu_count)
 try:
-    llm = Llama(model_path="./model/openllama-7b-300bt-ggml-q4_0.bin")
+    llm = Llama(model_path="./model/openllama-7b-300bt-ggml-q4_0.bin", n_threads=cpu_count)
 except:
     print("An exception occurred on loading model.")
 
@@ -22,7 +24,7 @@ def handler(event, context):
     else:
         data = json.loads(event['body'])
         print("data['prompt']", data['prompt'])
-        result = llm(data['prompt'], max_tokens=32,
+        result = llm(data['prompt'], max_tokens=256,
                      stop=["Q:", "\n"], echo=True)
         print(f"result: {result}")
         body = {
