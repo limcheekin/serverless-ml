4ae3a4b273982f1ed57283a16c472babdd7b3293
Lim Chee Kin <limcheekin@vobject.com>
2023-05-12 14:04:24 +0800

    feat: updated params setting

--- gpt4all-j/app.py
+++ gpt4all-j/app.py
@@ -1,9 +1,11 @@
 from gpt4allj import Model
-
 import json
+import multiprocessing
+import copy
 
 print('loading model...')
-
+cpu_count = multiprocessing.cpu_count()
+print("cpu_count", cpu_count)
 try:
     model = Model('./model/ggml-gpt4all-j')
 
@@ -14,11 +16,11 @@ print('model loaded\n')
 
 default_params: dict = {
     "seed": -1,
-    "n_threads": -1,
+    "n_threads": cpu_count,
     "n_predict": 200,
     "top_k": 40,
     "top_p": 0.9,
-    "temp": 0.9,
+    "temperature": 0.9,
     "repeat_penalty": 1,
     "repeat_last_n": 64,
     "n_batch":  8
@@ -34,23 +36,24 @@ def handler(event, context):
         body['message'] = 'WarmUP - Keep the Lambda warm!'
 
     else:
+        params = copy.deepcopy(default_params)
         data = json.loads(event['body'])
         print("prompt", data['prompt'])
         if 'params' in data:
             print("params", data['params'])
-            default_params.update(data['params'])
-            print("updated default_params", default_params)
+            params.update(data['params'])
+            print("updated params", params)
         result = model.generate(
             prompt=data['prompt'],
-            seed=default_params["seed"],
-            n_threads=default_params["n_threads"],
-            n_predict=default_params["n_predict"],
-            top_k=default_params["top_k"],
-            top_p=default_params["top_p"],
-            temp=default_params["temp"],
-            repeat_penalty=default_params["repeat_penalty"],
-            repeat_last_n=default_params["repeat_last_n"],
-            n_batch=default_params["n_batch"]
+            seed=params["seed"],
+            n_threads=params["n_threads"],
+            n_predict=params["n_predict"],
+            top_k=params["top_k"],
+            top_p=params["top_p"],
+            temp=params["temperature"],
+            repeat_penalty=params["repeat_penalty"],
+            repeat_last_n=params["repeat_last_n"],
+            n_batch=params["n_batch"]
         )
         print(f"result: {result}")
         body = {
