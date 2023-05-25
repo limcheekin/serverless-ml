e0d1d485b1d242e636e9676bd8526ca033ec4359
Lim Chee Kin <limcheekin@vobject.com>
2023-05-25 14:26:59 +0800

    chore: added missing models

--- flan-t5-xxl-ct2/download_flan-t5-xxl.sh
+++ flan-t5-xxl-ct2/download_flan-t5-xxl.sh
@@ -1,6 +1,9 @@
 mkdir -p google/flan-t5-xxl
 curl -L https://huggingface.co/google/flan-t5-xxl/resolve/main/pytorch_model-00001-of-00005.bin -o ./google/flan-t5-xxl/pytorch_model-00001-of-00005.bin
-curl -L https://huggingface.co/google/flan-t5-xxl/resolve/main/pytorch_model-00002-of-00002.bin -o ./google/flan-t5-xxl/pytorch_model-00002-of-00002.bin
+curl -L https://huggingface.co/google/flan-t5-xxl/resolve/main/pytorch_model-00002-of-00005.bin -o ./google/flan-t5-xxl/pytorch_model-00002-of-00005.bin
+curl -L https://huggingface.co/google/flan-t5-xxl/resolve/main/pytorch_model-00003-of-00005.bin -o ./google/flan-t5-xxl/pytorch_model-00003-of-00005.bin
+curl -L https://huggingface.co/google/flan-t5-xxl/resolve/main/pytorch_model-00004-of-00005.bin -o ./google/flan-t5-xxl/pytorch_model-00004-of-00005.bin
+curl -L https://huggingface.co/google/flan-t5-xxl/resolve/main/pytorch_model-00005-of-00005.bin -o ./google/flan-t5-xxl/pytorch_model-00005-of-00005.bin
 curl https://huggingface.co/google/flan-t5-xxl/raw/main/pytorch_model.bin.index.json -o ./google/flan-t5-xxl/pytorch_model.bin.index.json
 curl https://huggingface.co/google/flan-t5-xxl/raw/main/config.json -o ./google/flan-t5-xxl/config.json
 curl https://huggingface.co/google/flan-t5-xxl/raw/main/generation_config.json -o ./google/flan-t5-xxl/generation_config.json
