ef70073bed58c60ea6ed3904fe1e389e7721a5f7
Lim Chee Kin <limcheekin@vobject.com>
2023-05-26 15:01:28 +0800

    feat: added llama-cpp-python modal web app

---
+++ llama-cpp-python/fastapi_app.py
@@ -0,0 +1,16 @@
+# Modal Lab web app for llama.cpp.
+from modal import Image, Stub, asgi_app
+from llama_cpp.server.app import create_app
+
+stub = Stub("llama-cpp-python")
+
+image = Image.from_dockerfile("Dockerfile")
+
+
+@asgi_app()
+def fastapi_app():
+    return create_app()
+
+
+if __name__ == "__main__":
+    stub.deploy("webapp")
