47ef925ba93c6224913c510b41c2f364116d8bc6
Lim Chee Kin <limcheekin@vobject.com>
2023-05-26 09:30:20 +0800

    bugs: fixed indentation error

--- flan-t5-xxl-ct2/fastapi_app.py
+++ flan-t5-xxl-ct2/fastapi_app.py
@@ -13,10 +13,9 @@ stub = Stub("flan-t5-xxl-ct2")
 translator = None
 tokenizer = None
 
-# image = Image.debian_slim()
-image = Image.copy_local_file('download_flan-t5-xxl.sh')
-             .copy_local_file('hf_upload_model.py')
-             .from_dockerfile("Dockerfile")
+image = Image.copy_local_file('download_flan-t5-xxl.sh').copy_local_file(
+    'hf_upload_model.py').from_dockerfile("Dockerfile")
+
 
 class Response(BaseModel):
     completion: str
@@ -39,13 +38,14 @@ async def handle(request: Request, user_agent: Optional[str] = Header(None)):
         f"POST / - received user_agent={user_agent}, request.prompt={request.prompt}"
     )
     input_tokens = tokenizer.convert_ids_to_tokens(
-            tokenizer.encode(request.prompt))
+        tokenizer.encode(request.prompt))
     results = translator.translate_batch([input_tokens])
     output_tokens = results[0].hypotheses[0]
     result = tokenizer.decode(
-            tokenizer.convert_tokens_to_ids(output_tokens))
+        tokenizer.convert_tokens_to_ids(output_tokens))
     print(f"result: {result}")
-    return Response(completion=result) 
+    return Response(completion=result)
+
 
 def load_model():
     print('loading model...')
