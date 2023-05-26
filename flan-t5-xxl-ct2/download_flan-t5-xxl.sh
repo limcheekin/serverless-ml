a59811b45fa5330c9ea0581ee2fb516696a4a8b9
Lim Chee Kin <limcheekin@vobject.com>
2023-05-26 21:45:42 +0800

    chore: only pull related binary files

--- flan-t5-xxl-ct2/download_flan-t5-xxl.sh
+++ flan-t5-xxl-ct2/download_flan-t5-xxl.sh
@@ -1,3 +1,7 @@
 mkdir google
-git lfs install
+git lfs install --skip-smudge
 git clone https://huggingface.co/google/flan-t5-xxl google/flan-t5-xxl
+cd google/flan-t5-xxl
+git lfs pull --include "*.model"
+git lfs pull --include "*.bin"
+git lfs install --force
