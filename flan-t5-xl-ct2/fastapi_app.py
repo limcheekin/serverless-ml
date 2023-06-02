a154f8f328f2c567d23d6b366e4efe7922a630ea
Lim Chee Kin <limcheekin@vobject.com>
2023-06-02 17:16:11 +0800

    feat: deploy to modal.com instead of aws lambda

---
+++ flan-t5-xl-ct2/fastapi_app.py
@@ -0,0 +1,79 @@
+from typing import Optional
+
+from fastapi import FastAPI, Header
+from pydantic import BaseModel
+
+from modal import Image, Stub, asgi_app, Mount
+import os
+
+web_app = FastAPI()
+stub = Stub("flan-t5-xl-ct2")
+image = Image.from_dockerfile("Dockerfile", context_mount=Mount.from_local_dir(
+    ".", remote_path=".")).env({"MODEL": os.environ["MODEL"]})
+stub.image = image
+
+if stub.is_inside(stub.image):
+    from transformers import AutoTokenizer
+    import ctranslate2
+    import os
+    print(f"loading {os.environ['MODEL']} model...")
+    translator = ctranslate2.Translator(os.environ["MODEL"])
+    tokenizer = AutoTokenizer.from_pretrained(os.environ["MODEL"])
+    print('model loaded\n')
+
+
+class Response(BaseModel):
+    prompt: str
+
+
+class Request(BaseModel):
+    prompt: str
+    params: Optional[dict] = {
+        "beam_size": 2,
+        "top_k": 1,              # sampling_topk
+        "temperature": 1.0,      # sampling_temperature
+        "repeat_penalty": 1.0,   # repetition_penalty
+        "no_repeat_ngram_size": 0,
+        "min_length": 1,         # min_decoding_length
+        "max_length": 256,       # max_decoding_length
+    }
+
+
+@web_app.get("/")
+async def handle_root(user_agent: Optional[str] = Header(None)):
+    print(f"GET /     - received user_agent={user_agent}")
+    return "Hello World"
+
+
+@web_app.post("/")
+async def handle(request: Request, user_agent: Optional[str] = Header(None)):
+    print(
+        f"POST / - received user_agent={user_agent}, request.prompt={request.prompt}, request.params={request.params}"
+    )
+    input_tokens = tokenizer.convert_ids_to_tokens(
+        tokenizer.encode(request.prompt))
+    results = translator.translate_batch(
+        [input_tokens],
+        beam_size=request.params["beam_size"],
+        sampling_topk=request.params["top_k"],
+        sampling_temperature=request.params["temperature"],
+        repetition_penalty=request.params["repeat_penalty"],
+        no_repeat_ngram_size=request.params["no_repeat_ngram_size"],
+        min_decoding_length=request.params["min_length"],
+        max_decoding_length=request.params["max_length"],
+    )
+    output_tokens = results[0].hypotheses[0]
+    result = tokenizer.decode(
+        tokenizer.convert_tokens_to_ids(output_tokens))
+    print(f"result: {result}")
+    return Response(prompt=result)
+
+
+@stub.function(image=image)
+@asgi_app()
+def fastapi_app():
+    return web_app
+
+
+if __name__ == "__main__":
+    stub.deploy("webapp")
--- flan-t5-xl-ct2/hf_upload_model.py
+++ flan-t5-xl-ct2/hf_upload_model.py
@@ -1,11 +1,14 @@
 from huggingface_hub import HfApi, create_repo
 import os
 
+with open('token.txt', 'r') as f:
+    token = f.read().rstrip("\n")
+
 create_repo("limcheekin/flan-t5-xl-ct2",
-            token=os.environ["HF_TOKEN"],
+            token=token,
             repo_type="model",
             exist_ok=True)
-api = HfApi(token=os.environ["HF_TOKEN"])
+api = HfApi(token=token)
 api.upload_folder(
     folder_path="google/flan-t5-xl-ct2",
     repo_id="limcheekin/flan-t5-xl-ct2",
