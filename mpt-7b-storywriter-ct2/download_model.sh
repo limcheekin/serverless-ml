b07f313fb49db27c4c2328a8d16756bf182994d8
Lim Chee Kin <limcheekin@vobject.com>
2023-06-02 14:52:27 +0800

    feat: added download EleutherAI/gpt-neox-20b tokenizer files

--- mpt-7b-storywriter-ct2/download_model.sh
+++ mpt-7b-storywriter-ct2/download_model.sh
@@ -5,3 +5,10 @@ cd mosaicml/mpt-7b-storywriter
 git lfs pull
 pwd
 ls -l
+
+mkdir -p EleutherAI/gpt-neox-20b
+curl https://huggingface.co/EleutherAI/gpt-neox-20b/raw/main/tokenizer_config.json -o EleutherAI/gpt-neox-20b/tokenizer_config.json
+curl https://huggingface.co/EleutherAI/gpt-neox-20b/raw/main/vocab.json -o EleutherAI/gpt-neox-20b/vocab.json
+curl https://huggingface.co/EleutherAI/gpt-neox-20b/raw/main/merges.txt -o EleutherAI/gpt-neox-20b/merges.txt
+curl https://huggingface.co/EleutherAI/gpt-neox-20b/resolve/main/tokenizer.json -o EleutherAI/gpt-neox-20b/tokenizer.json
+curl https://huggingface.co/EleutherAI/gpt-neox-20b/resolve/main/special_tokens_map.json -o EleutherAI/gpt-neox-20b/special_tokens_map.json
--- mpt-7b-storywriter-ct2/fastapi_app.py
+++ mpt-7b-storywriter-ct2/fastapi_app.py
@@ -16,12 +16,12 @@ if stub.is_inside(stub.image):
     import ctranslate2
     print('loading model...')
     generator = ctranslate2.Generator("/mosaicml/mpt-7b-storywriter-ct2")
-    tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")
+    tokenizer = AutoTokenizer.from_pretrained("/EleutherAI/gpt-neox-20b")
     print('model loaded.')
 
 
 class Response(BaseModel):
-    completion: str
+    prompt: str
 
 
 class Request(BaseModel):
@@ -38,7 +38,7 @@ async def handle_root(user_agent: Optional[str] = Header(None)):
 @web_app.post("/")
 async def handle(request: Request, user_agent: Optional[str] = Header(None)):
     print(
-        f"POST / - received user_agent={user_agent}, request.prompt={request.prompt}"
+        f"POST / - received user_agent={user_agent}, request.prompt={request.prompt}, request.params={request.params}"
     )
     input_tokens = tokenizer.convert_ids_to_tokens(
         tokenizer.encode(request.prompt))
@@ -46,7 +46,7 @@ async def handle(request: Request, user_agent: Optional[str] = Header(None)):
         [input_tokens], max_length=256, sampling_topk=10)
     text = tokenizer.decode(results[0].sequences_ids[0])
     print(f"text: {text}")
-    return Response(completion=text)
+    return Response(prompt=text)
 
 
 @stub.function(image=image)
