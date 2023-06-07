68e0647c6e356f294480be89bab75193cd882747
Lim Chee Kin <limcheekin@vobject.com>
2023-06-07 20:44:30 +0800

    feat: set cpu core to 14 max

--- starcoder-ct2/fastapi_app.py
+++ starcoder-ct2/fastapi_app.py
@@ -52,7 +52,7 @@ async def handle(request: Request, user_agent: Optional[str] = Header(None)):
     return Response(prompt=result)
 
 
-@stub.function(image=image, cpu=42, timeout=600)
+@stub.function(image=image, cpu=14, timeout=600)
 @asgi_app()
 def fastapi_app():
     return web_app
