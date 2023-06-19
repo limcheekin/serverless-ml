bb173e4ebb8e537310057c3c78bef754b63fb72c
Lim Chee Kin <limcheekin@vobject.com>
2023-06-19 16:40:38 +0800

    feat: added timeout 600s

--- falcon-7b-instruct-ct2/fastapi_app.py
+++ falcon-7b-instruct-ct2/fastapi_app.py
@@ -52,7 +52,7 @@ async def handle(request: Request, user_agent: Optional[str] = Header(None)):
     return Response(prompt=result)
 
 
-@stub.function(image=image)
+@stub.function(image=image, timeout=600)
 @asgi_app()
 def fastapi_app():
     return web_app
