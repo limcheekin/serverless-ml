40b1dd4189d8dd289ae3da6a48f06a86a96fc9ec
Lim Chee Kin <limcheekin@vobject.com>
2023-06-27 11:37:13 +0800

    feat: added cpu and memory settings

--- llama-cpp-python/fastapi_app.py
+++ llama-cpp-python/fastapi_app.py
@@ -7,7 +7,7 @@ image = Image.from_dockerfile("Dockerfile", force_build=True).env(
     {"MODEL": "/model/ggml-model-q4_0.bin"})
 
 
-@stub.function(image=image, keep_warm=1, timeout=900)
+@stub.function(image=image, cpu=14, memory=20480, keep_warm=1, timeout=900)
 @asgi_app()
 def fastapi_app():
     from llama_cpp.server.app import create_app
