074237e02fd2cc21e8e0af541a58077eac1ad51f
Lim Chee Kin <limcheekin@vobject.com>
2023-06-27 14:31:45 +0800

    feat: updated mpt-7b-storywriter-ct2 code for retest

--- mpt-7b-storywriter-ct2/download_model.sh
+++ mpt-7b-storywriter-ct2/download_model.sh
@@ -7,13 +7,13 @@ git lfs install --force
 pwd
 ls -l
 
-mkdir -p /EleutherAI/gpt-neox-20b
-curl https://huggingface.co/EleutherAI/gpt-neox-20b/resolve/main/config.json -o /EleutherAI/gpt-neox-20b/config.json
-curl https://huggingface.co/EleutherAI/gpt-neox-20b/resolve/main/tokenizer_config.json -o /EleutherAI/gpt-neox-20b/tokenizer_config.json
-curl https://huggingface.co/EleutherAI/gpt-neox-20b/resolve/main/vocab.json -o /EleutherAI/gpt-neox-20b/vocab.json
-curl https://huggingface.co/EleutherAI/gpt-neox-20b/resolve/main/merges.txt -o /EleutherAI/gpt-neox-20b/merges.txt
-curl https://huggingface.co/EleutherAI/gpt-neox-20b/resolve/main/tokenizer.json -o /EleutherAI/gpt-neox-20b/tokenizer.json
-curl https://huggingface.co/EleutherAI/gpt-neox-20b/resolve/main/special_tokens_map.json -o /EleutherAI/gpt-neox-20b/special_tokens_map.json
-cd /EleutherAI/gpt-neox-20b
-pwd
-ls -l
+#mkdir -p /EleutherAI/gpt-neox-20b
+#curl https://huggingface.co/EleutherAI/gpt-neox-20b/resolve/main/config.json -o /EleutherAI/gpt-neox-20b/config.json
+#curl https://huggingface.co/EleutherAI/gpt-neox-20b/resolve/main/tokenizer_config.json -o /EleutherAI/gpt-neox-20b/tokenizer_config.json
+#curl https://huggingface.co/EleutherAI/gpt-neox-20b/resolve/main/vocab.json -o /EleutherAI/gpt-neox-20b/vocab.json
+#curl https://huggingface.co/EleutherAI/gpt-neox-20b/resolve/main/merges.txt -o /EleutherAI/gpt-neox-20b/merges.txt
+#curl https://huggingface.co/EleutherAI/gpt-neox-20b/resolve/main/tokenizer.json -o /EleutherAI/gpt-neox-20b/tokenizer.json
+#curl https://huggingface.co/EleutherAI/gpt-neox-20b/resolve/main/special_tokens_map.json -o /EleutherAI/gpt-neox-20b/special_tokens_map.json
+#cd /EleutherAI/gpt-neox-20b
+#pwd
+#ls -l
--- mpt-7b-storywriter-ct2/fastapi_app.py
+++ mpt-7b-storywriter-ct2/fastapi_app.py
@@ -1,32 +1,32 @@
 from typing import Optional
 
-from fastapi import FastAPI, Header
+from fastapi import FastAPI, Header, Request
 from pydantic import BaseModel
 
 from modal import Image, Stub, asgi_app, Mount
+import os
 
+model_name = os.environ["MODEL"]
+index = model_name.rindex("/") + 1
+stub_name = model_name[index:]
 web_app = FastAPI()
-stub = Stub("mpt-7b-storywriter-ct2")
+stub = Stub(stub_name)
 image = Image.from_dockerfile("Dockerfile", context_mount=Mount.from_local_dir(
-    ".", remote_path="."), force_build=True)
+    ".", remote_path="."), force_build=True).env({"MODEL": model_name})
 stub.image = image
 
 if stub.is_inside(stub.image):
     from transformers import AutoTokenizer
     import ctranslate2
-    print('loading model...')
-    generator = ctranslate2.Generator("/mosaicml/mpt-7b-storywriter-ct2")
-    tokenizer = AutoTokenizer.from_pretrained("/EleutherAI/gpt-neox-20b")
-    print('model loaded.')
+    import os
+    print(f"loading {os.environ['MODEL']} model...")
+    generator = ctranslate2.Generator(os.environ["MODEL"])
+    tokenizer = AutoTokenizer.from_pretrained(os.environ["MODEL"])
+    print('model loaded\n')
 
 
 class Response(BaseModel):
     prompt: str
-
-
-class Request(BaseModel):
-    prompt: str
-    params: Optional[dict] = None
 
 
 @web_app.get("/")
@@ -37,19 +37,22 @@ async def handle_root(user_agent: Optional[str] = Header(None)):
 
 @web_app.post("/")
 async def handle(request: Request, user_agent: Optional[str] = Header(None)):
+    data = await request.json()
     print(
-        f"POST / - received user_agent={user_agent}, request.prompt={request.prompt}, request.params={request.params}"
+        f"POST / - received user_agent={user_agent}, request.json()={data}"
     )
+    prompt = data.pop("prompt")
+    params = data
     input_tokens = tokenizer.convert_ids_to_tokens(
-        tokenizer.encode(request.prompt))
-    results = generator.generate_batch(
-        [input_tokens], max_length=256, sampling_topk=10)
-    text = tokenizer.decode(results[0].sequences_ids[0])
-    print(f"text: {text}")
-    return Response(prompt=text)
+        tokenizer.encode(prompt))
+    results = generator.generate_batch([input_tokens], **params)
+    output_tokens = results[0].sequences_ids[0]
+    result = tokenizer.decode(output_tokens)
+    print(f"result: {result}")
+    return Response(prompt=result)
 
 
-@stub.function(image=image)
+@stub.function(image=image, cpu=14, memory=20480, timeout=900)
 @asgi_app()
 def fastapi_app():
     return web_app
