ca9e0a73e429eaedec2483abc304d1c4acc04e9b
Lim Chee Kin <limcheekin@vobject.com>
2023-07-22 13:05:19 +0800

    feat: updated to use michaelfeil/ct2fast-Llama-2-13b-chat-hf model

--- llama-2-13b-chat-ct2/download_model.sh
+++ llama-2-13b-chat-ct2/download_model.sh
@@ -1,7 +1,7 @@
 mkdir michaelfeil
 git lfs install --skip-smudge
-git clone https://huggingface.co/michaelfeil/ct2fast-Llama-2-13b-hf michaelfeil/ct2fast-Llama-2-13b-hf
-cd michaelfeil/ct2fast-Llama-2-13b-hf
+git clone https://huggingface.co/michaelfeil/ct2fast-Llama-2-13b-chat-hf michaelfeil/ct2fast-Llama-2-13b-chat-hf
+cd michaelfeil/ct2fast-Llama-2-13b-chat-hf
 git lfs pull
 git lfs install --force
 pwd
