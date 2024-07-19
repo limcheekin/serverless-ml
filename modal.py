e88f4b4aab1e6d236c1dd8dfcc6f6b85e64f0da2
Lim Chee Kin <limcheekin@vobject.com>
2024-07-19 15:18:19 +0800

    feat: added modal app code and updated workflow

---
+++ modal.py
@@ -0,0 +1,37 @@
+# Modal Lab web app for open-text-embeddings.
+from modal import Image, App, asgi_app
+import os
+
+app = App(os.environ["APP_NAME"])
+
+image = Image.from_registry("python:3.11-slim-bookworm"
+        ).copy_local_file(f"{os.environ['APP_NAME']}.guff"
+        ).dockerfile_commands(
+            "RUN apt-get update && apt-get install -y libopenblas-dev ninja-build build-essential pkg-config",
+            'RUN CMAKE_ARGS="-DLLAMA_BLAS=ON -DLLAMA_BLAS_VENDOR=OpenBLAS" FORCE_CMAKE=1 pip install --no-cache-dir llama-cpp-python[server]'
+        ).env({
+            "APP_NAME": os.environ["APP_NAME"],
+        })
+
+
+cpu = float(os.getenv("CPU", 2.0))
+memory = int(os.getenv("MEMORY", 2048)) 
+timeout = int(os.getenv("TIMEOUT", 600))
+keep_warm = int(os.getenv("KEEP_WARM", 1))
+@app.function(image=image, cpu=cpu, memory=memory, timeout=timeout, keep_warm=keep_warm)
+@asgi_app()
+def fastapi_app():
+    from llama_cpp.server.app import create_app, Settings
+    import os
+    print("os.cpu_count()", os.cpu_count())
+    app = create_app(
+        Settings(
+            n_threads=os.cpu_count(),
+            model=f"./{os.environ["APP_NAME"]}.guff"
+        )
+    )
+    return app
+
+
+if __name__ == "__main__":
+    app.deploy("webapp")
